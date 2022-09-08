584846e03d7c000fe194a7bc059f2499cf38bf1d
James Alhassan <jamesalhassan052@gmail.com>
2022-09-08 17:32:28 +0000

    6-size.c script updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %c byte(s)\n", sizeof(a));
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
 printf("Size of an int: %d byte(s)\n", sizeof(b));
 printf("Size of a long int: %ld byte(s)\n", sizeof(c));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
-printf("Size of a float: %g byte(s)\n", sizeof(e));
+printf("Size of a float: %lu byte(s)\n", sizeof(e));
 
 return (0);
 }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -14,4 +14,4 @@
 
 5-printf.c script - prints the string "with proper grammar, but the outcome is a piece of art" using printf
 
-6-size.c script - prints the sizes of various data types 
+6-size.c script - prints the sizes of various data types on different systems  
