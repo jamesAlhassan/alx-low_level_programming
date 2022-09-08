54f454b6b5b64801ce93b9c7a76bbbafc9313573
James Alhassan <jamesalhassan052@gmail.com>
2022-09-08 17:14:37 +0000

    6-size.c script added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ long int c;
 long long int d;
 float e;
 
-puts("Size of a char: %c byte(s)", sizeof(a));
-puts("Size of an int: %d byte(s)", sizeof(b));
-puts("Size of a long int: %ld byte(s)", sizeof(c));
-puts("Size of a long long int: %lld byte(s)", sizeof(d));
-puts("Size of a long int: %f byte(s)", sizeof(e));
+printf("Size of a char: %c byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lld byte(s)\n", sizeof(d));
+printf("Size of a long int: %f byte(s)\n", sizeof(e));
 
 return (0);
 }
