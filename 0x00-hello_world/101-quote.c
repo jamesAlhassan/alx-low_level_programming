9305a296a4bc1efa535d366fa73bcd341276edc5
James Alhassan <jamesalhassan052@gmail.com>
2022-09-08 19:59:16 +0000

    101-quote.c script added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Prints the string to stderr
+ * Return: 0 if success
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
+
+
