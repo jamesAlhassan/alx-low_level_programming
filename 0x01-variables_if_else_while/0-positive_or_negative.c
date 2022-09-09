b76c30ba4e996fdf057b4be018a33095ceda97bb
James Alhassan <jamesalhassan052@gmail.com>
2022-09-09 21:30:59 +0000

    0-positive_or_negative.c script added

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Checks if positive,zero or negative 
+ *return : 0 if success
+ */
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+puts("is positive");
+}
+else if (n == 0)
+{
+puts("is zero");
+}
+else
+{
+puts("is negative")
+}
+return (0);
+}
