c2c4c173bdf895caf07364249d2e9aa2f27a3c2e
James Alhassan <jamesalhassan052@gmail.com>
2022-09-09 23:09:03 +0000

    2-print_alphabet.c script added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets in lower case
+ * Return: 0 if success
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+
+{
+putchar(alphabet);
+}
+return (0);
+}
