a56e7ee2b699302ec468e5e9f4858a7c569b6c15
James Alhassan <jamesalhassan052@gmail.com>
2022-09-10 00:33:11 +0000

    5-print_numbers.c script added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints digits from 0 -9
+ * Return: 0 if success
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++;)
+{
+puts("%d", num);
+}
+return (0);
+}
