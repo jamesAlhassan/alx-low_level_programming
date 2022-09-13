6a237a4e72c794ba8d8ea76f149084ab079648fb
James Alhassan <jamesalhassan052@gmail.com>
2022-09-13 22:09:05 +0000

    0-putchar.c script added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - Prints to stdout
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+char _ = '_';
+char p = 'p';
+char u = 'u';
+char t = 't';
+char c = 'c';
+char h = 'h';
+char a = 'a';
+char r = 'r';
+
+_putchar(_);
+_putchar(p);
+_putchar(u);
+_putchar(t);
+_putchar(c);
+_putchar(h);
+_putchar(a);
+_putchar(r);
+_putchar('\n');
+
+return (0);
+}
+
+
