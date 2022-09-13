874b7f005c9e6cf27b8ea5b79bb2b5f6fc51fc63
James Alhassan <jamesalhassan052@gmail.com>
2022-09-13 23:36:42 +0000

    1-alphabet.c script addded

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints lower case alphabets
+ *
+ * Return: void
+ */
+
+
+
+void print_alphabet(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+}
+
