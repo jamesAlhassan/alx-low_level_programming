ff7c74f9324f873c0b9059b9f4d5ccdc7e80c619
James Alhassan <jamesalhassan052@gmail.com>
2022-09-15 06:45:27 +0000

    README,  0-main.c and  main.h added

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests function if 0 passed to positive_or_negative()
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+
+}
+
