347b7f85dce11424282a01236b8d44e22352a9ee
James Alhassan <jamesalhassan052@gmail.com>
2022-09-15 23:18:03 +0000

    2-mul.c script added and README updated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first parameters
+ * @b: second parameter
+ * Return: 0 if success
+ */
+
+int mul(int a, int b)
+{
+int multi = a * b;
+return (multi);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -19,3 +19,4 @@ Don’t forget to push your header file
 
 ## 0-isupper.c- checks for uppercase character
 ## 1-isdigit.c -  checks for a digit (0 through 9).
+## 2-mul.c - multiplies two integers.
