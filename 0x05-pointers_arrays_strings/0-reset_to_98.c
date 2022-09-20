68d00ee7baedd41731cb9f42a17a31120b016033
James Alhassan <jamesalhassan052@gmail.com>
2022-09-20 22:00:59 +0000

    0-reset_to_98.c script and README added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+ * reset_to_98 - takes a pointer and updates the value it points to to 98
+ * @n: pointer to pass
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x05-pointers_arrays_strings Directory
+
+0-reset_to_98.c script takes a pointer to an int as parameter and updates the value it points to to 98.- 
