dd1059a59833e1475ccdaecbcf9d19722c7701a2
James Alhassan <jamesalhassan052@gmail.com>
2022-09-27 21:28:48 +0000

    7-print_chessboard.c script added

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,23 +1,18 @@
+#include "main.h"
 /**
- * print_chessboard - prints the chessboard.
- * @a: board to print
+ * print_chessboard - print a chessboar
+ * @a: number of rows
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	i = j = 0;
-	while (i < 8)
+	for (i = 0; i < 8; i++)
 	{
-		j = 0;
-		while (j < 8)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			j++;
 		}
-		_putchar('\n');
-		i++;
+		_putchar(10);
 	}
-
 }
