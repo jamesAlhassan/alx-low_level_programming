602704ffbe37ac357743281b5cd59e7342f4fecb
James Alhassan <jamesalhassan052@gmail.com>
2022-09-27 04:50:40 +0000

    3-strspn.c script added

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: strin to be scanned
+ * @accept: string containing th characters to match s
+ * Return: number of matching initial characters
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0' && s[i] != accept[j])
+			j++;
+		if (accept[j] == '\0')
+			return (i);
+		i++;
+	}
+	return (i);
+}
