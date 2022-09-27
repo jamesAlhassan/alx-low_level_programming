cd557ecab7dde78a31d296498494c4fdf152b63a
James Alhassan <jamesalhassan052@gmail.com>
2022-09-27 21:17:28 +0000

    5-strstr.c script added

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,7 +10,7 @@ char *_strstr(char *haystack, char *needle)
 	int i, j;
 
 	for (i = 0; haystack[i] > '\0'; i++)
-{
+	{
 		for (j = i; haystack[j] > '\0' && needle[j - i] > '0'; j++)
 		{
 			if (haystack[j] != needle[j - i])
@@ -22,7 +22,7 @@ char *_strstr(char *haystack, char *needle)
 		{
 			return (haystack + i);
 		}
-}
+	}
 
 	return (0);
 }
