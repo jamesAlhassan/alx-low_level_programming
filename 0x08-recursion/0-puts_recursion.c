e58dce567df061311f95053c521b9dd57cf17b2a
James Alhassan <jamesalhassan052@gmail.com>
2022-09-28 20:26:45 +0000

    0-puts_recursion.c script added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "_putchar.c"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: pointer parameter
+ */
+
+void _puts_recursion(char *s)
+{
+        if (*s == '\0')
+            _putchar('\n');
+        else
+        {
+            _putchar(*s);
+            _puts_recursion(++s);
+        }
+}
