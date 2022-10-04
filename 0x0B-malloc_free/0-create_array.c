cfca166b18e50560af6249ea987f6e99927ca256
James Alhassan <jamesalhassan052@gmail.com>
2022-10-04 03:31:48 +0000

    0-create_array.c script  updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,9 @@ char *create_array(unsigned int size, char c)
 
 	ptr = (char *) malloc(size * sizeof(char));
 
+	if (sizeof(ptr) == 0)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
