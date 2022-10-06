30ab76b1ff54a1edd39f3d0395e585454879a1f8
James Alhassan <jamesalhassan052@gmail.com>
2022-10-06 03:45:45 +0000

    100-argstostr.c script added

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments 
+ * @ac: number of arguments
+ * av: strings to concatenate
+ * Return: pointer to the new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	
+}
+
+
