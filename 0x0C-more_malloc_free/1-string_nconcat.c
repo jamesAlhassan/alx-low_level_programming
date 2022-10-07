960f3f00e5f0c1aa46224c8e84e06a5a260fb867
James Alhassan <jamesalhassan052@gmail.com>
2022-10-07 04:49:27 +0000

    1-string_nconcat.c script updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,34 +7,29 @@
  * @n: first number of s2 bytes
  * Return: pointer to concatenated strimg s1, NULL if fails
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_Len, s2_Len, i;
 	char *ptr;
+	unsigned int s1_Len, s2_Len, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (s1_Len = 0; s1[s1_Len] != '\0'; s1_Len++)
+	for (s1_Len = 0; s1[s1_Len]; s1_Len++)
 		;
-
-	for (s2_Len = 0; s2[s2_Len] != '\0'; s2_Len++)
+	for (s2_Len = 0; s2[s2_Len]; s2_Len++)
 		;
+	s2_Len > n ? (s2_Len = n) : (n = s2_Len);
 
-	n > s2_Len || n == s2_Len ? (n = s2_Len) : (s2_Len = n);
-
-	ptr = malloc(s1_Len * sizeof(s1) + s2_Len * sizeof(s2));
+	ptr = malloc((s1_Len + s2_Len + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < (s1_Len + s2_Len + 1); i++)
-	{
+	for (i = 0; i < (s1_Len + s2_Len + 1) - 1; i++)
 		i < s1_Len ? (ptr[i] = s1[i]) : (ptr[i] = s2[i - s1_Len]);
-	}
 	ptr[(s1_Len + s2_Len + 1)] = '\0';
-
 	return (ptr);
 }
+
