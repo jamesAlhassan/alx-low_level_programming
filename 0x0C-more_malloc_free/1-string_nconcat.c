36d3503c158702f9d3d137f37f0319260884b4a8
James Alhassan <jamesalhassan052@gmail.com>
2022-10-07 05:43:25 +0000

    1-string_nconcat.c script updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,20 +24,18 @@ int _strlen(char *string)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int num, len, i, j;
-
-	num = n;
+	int len, i, j;
 
 	if (s1 == NULL) /* account for NULL strings */
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (num < 0) /* account for negative n bytes */
+	if (n < 0) /* account for negative n bytes */
 		return (NULL);
-	if (num >= _strlen(s2)) /* account for n too big */
-		num = _strlen(s2);
+	if (n >= _strlen(s2)) /* account for n too big */
+		n = _strlen(s2);
 
-	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+	len = _strlen(s1) + n + 1; /* +1 to account for null pointer */
 
 	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
 	if (ptr == NULL)
@@ -45,7 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i] != '\0'; i++) /* concat */
 		ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
+	for (j = 0; j < n; j++)
 		ptr[i + j] = s2[j];
 	ptr[i + j] = '\0';
 
