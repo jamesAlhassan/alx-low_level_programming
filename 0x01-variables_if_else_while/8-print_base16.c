443adc2784262eca122cb2492e6e34b8f0bb8cbd
James Alhassan <jamesalhassan052@gmail.com>
2022-10-09 08:49:19 +0000

    8-print_base16.c  updated

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
+
 /**
  * main - Prints numbers of base 16
  * Return: 0 if success
  */
+
 int main(void)
 {
-int hexdec = '0';
-while (hexdec <= '9')
-{
-putchar(hexdec);
-hexdec++;
-}
-hexdec = 'a';
-while (hexdec <= 'f')
-{
-putchar(hexdec);
-hexdec++;
-}
-putchar('\n');
-return (0);
+	int hexdec = '0';
+
+	while (hexdec <= '9')
+	{
+		putchar(hexdec);
+		hexdec++;
+	}
+
+	hexdec = 'a';
+
+	while (hexdec <= 'f')
+	{
+		putchar(hexdec);
+		hexdec++;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
