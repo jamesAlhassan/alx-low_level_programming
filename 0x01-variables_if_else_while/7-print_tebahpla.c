03cf67a6c367f1a60a89b6bed24a6682e2e9e1ba
James Alhassan <jamesalhassan052@gmail.com>
2022-10-09 08:45:12 +0000

    7-print_tebahpla.c  updated

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,15 @@
  */
 int main(void)
 {
-char z = 'z';
-while (z >= 'a')
-{
-putchar(z);
-z--;
-}
-putchar('\n');
-return (0);
+	char z = 'z';
+
+	while (z >= 'a')
+	{
+		putchar(z);
+		z--;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
