72ee72ec7684d04ad676cc74788a1046f4065d85
James Alhassan <jamesalhassan052@gmail.com>
2022-10-09 08:42:01 +0000

    6-print_numberz.c  updated

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,16 @@
  */
 int main(void)
 {
-int num = '0';
-while (num <= '9')
-{
-putchar(num);
-num++;
-}
-putchar('\n');
-return (0);
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
 }
