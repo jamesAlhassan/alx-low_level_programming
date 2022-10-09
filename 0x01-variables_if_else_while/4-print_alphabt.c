38203a4f95177c967d6e94c8107eb32c243832a6
James Alhassan <jamesalhassan052@gmail.com>
2022-10-09 04:36:51 +0000

    4-print_alphabt.c updated

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,17 +3,19 @@
  * main - Prints alphabets excluding q and e in  lower case
  * Return: 0 if success
  */
-int main(void)
-{
-char alphabet = 'a';
-while (alphabet <= 'z')
-{
-if (alphabet != 'q' && alphabet != 'e')
-{
-putchar(alphabet);
-}
-alphabet++;
-}
-putchar('\n');
-return (0);
-}
+	int main(void)
+	{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+		alphabet++;
+		}
+	putchar('\n');
+
+	return (0);
+	}
