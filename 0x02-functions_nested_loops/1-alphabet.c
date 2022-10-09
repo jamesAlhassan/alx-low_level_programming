55453853734bac49371b16319d7063f87ede3901
James Alhassan <jamesalhassan052@gmail.com>
2022-10-09 16:00:06 +0000

    1-alphabet.c updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,16 +6,16 @@
  * Return: void
  */
 
-
-
 void print_alphabet(void)
 {
-char a = 'a';
-while (a <= 'z')
-{
-_putchar(a);
-a++;
-}
-_putchar('\n');
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+
+	_putchar('\n');
 }
 
