750d98c15a4ef94126d1aa43318f4155724859ef
James Alhassan <jamesalhassan052@gmail.com>
2022-10-09 16:29:35 +0000

    4-isalpha.c updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,16 +8,16 @@
 
 int _isalpha(int c)
 {
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
-else if (c >= 97 && c <= 122)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
