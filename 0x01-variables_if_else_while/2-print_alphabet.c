cdce5b57ee676a0998a4df390dcb67ae3a05c309
James Alhassan <jamesalhassan052@gmail.com>
2022-10-09 04:41:04 +0000

    2-print_alphabet.c updated

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,13 @@
  */
 int main(void)
 {
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-putchar(alphabet);
-}
-putchar('\n');
-return (0);
-}
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
+	}
