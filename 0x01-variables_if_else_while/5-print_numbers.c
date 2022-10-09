36d960b188cd88b12cdc1a0005c1feacb45fb637
James Alhassan <jamesalhassan052@gmail.com>
2022-10-09 08:35:46 +0000

    5-print_numbers.c  updated

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,14 @@
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-{
-printf("%d", num);
-}
-putchar('\n');
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+
+	putchar('\n');
+
 return (0);
 }
