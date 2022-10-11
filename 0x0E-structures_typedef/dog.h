e669fafb21dc42ee6c4442f4514a83d771bbfb01
James Alhassan <jamesalhassan052@gmail.com>
2022-10-11 05:02:21 +0000

    README dog.h updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - a dos's profile
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char * name;
 	float age;
 	char* owner;
 };
+
+#endif
