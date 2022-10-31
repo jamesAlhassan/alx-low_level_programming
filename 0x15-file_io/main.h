5ffa81f8b7e75b92c06912b7e598f54c845df4ce
James Alhassan <jamesalhassan052@gmail.com>
2022-10-31 22:50:18 +0000

    main.h added

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+#endif
