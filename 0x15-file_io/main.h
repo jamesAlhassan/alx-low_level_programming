2cf7c8d4b7f29ea3e3ef5b802187aaf4578f72a8
James Alhassan <jamesalhassan052@gmail.com>
2022-11-02 05:54:23 +0000

    main.h updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,7 +18,10 @@ void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
-
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
 
 
 
