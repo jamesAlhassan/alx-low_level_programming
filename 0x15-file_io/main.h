de4cf4d6a3a12eee995543b6a6e1adc049cc613a
James Alhassan <jamesalhassan052@gmail.com>
2022-11-02 05:53:11 +0000

    main.h updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,5 +14,13 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 int _strlen(char *str);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+
+
+
+
+
+
 #endif
 
