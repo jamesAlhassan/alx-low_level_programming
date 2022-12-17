92eeeec33f9caaf09d143950b81d3c3f2ce4b266
James Alhassan <jamesalhassan052@gmail.com>
2022-12-17 06:08:00 +0000

    liball

---
+++ 0x18-dynamic_libraries/funcs/5-strstr.c
@@ -0,0 +1,28 @@
+/**
+ * _strstr - locates a substring
+ * finds the occurance of the substring needle in the string haystack
+ * @haystack: string to search in
+ * @needle: string to search
+ * Return: a pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] > '\0'; i++)
+	{
+		for (j = i; haystack[j] > '\0' && needle[j - i] > '0'; j++)
+		{
+			if (haystack[j] != needle[j - i])
+			{
+				break;
+			}
+		}
+		if (needle[j - i] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+
+	return (0);
+}
