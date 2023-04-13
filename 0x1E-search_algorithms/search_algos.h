578fbb25aeca9d6f0f2a776c21540b04f3da2312
James Alhassan <jamesalhassan052@gmail.com>
2023-04-13 20:57:31 +0000

    Update 0-search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
-ifndef SORT_ALG
-define SORT_ALG
+#ifndef SORT_ALG_
+#define SORT_ALG_
+#include <stdlib.h>
+#include <stdio.h>
+
 
 int linear_search(int *array, size_t size, int value);
 
-endif
+#endif
