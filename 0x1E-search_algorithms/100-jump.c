14ab7b7ead61d22db79995347817dc76ae9198e3
James Alhassan <jamesalhassan052@gmail.com>
2023-04-17 05:21:34 +0000

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,3 +13,28 @@ size_t min(size_t a, size_t b)
 	return (a <= b ? a : b);
 }
 
+/**
+ * jump_search - searches for a value in a sorted array of integers using
+ * a jump search algorithm
+ * @array: pointer to first element of array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: first index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t a, b, jump;
+
+	if (!array || size == 0)
+		return (-1);
+
+	jump = sqrt(size);
+
+	for (b = 0; b < size && array[b] < value;
+	     a = b, b += jump)
+	{
+		printf("Value checked array[%lu] = [%d]\n",
+		       b, array[b]);
