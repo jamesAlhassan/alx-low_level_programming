8bee432db4e735fd57f25c18978f75f441edf0c2
James Alhassan <jamesalhassan052@gmail.com>
2023-04-17 05:19:47 +0000

    Add 100-jump.c

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * min - returns the minimum of two size_t values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: `a` if lower or equal to `b`, `b` otherwise
+ */
+size_t min(size_t a, size_t b)
+{
+	return (a <= b ? a : b);
+}
+
