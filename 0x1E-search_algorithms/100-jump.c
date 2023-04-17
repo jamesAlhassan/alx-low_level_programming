a78b392f518d021a1cbc49a2f8be0de9373e0f68
James Alhassan <jamesalhassan052@gmail.com>
2023-04-17 05:23:51 +0000

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -38,3 +38,16 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%lu] = [%d]\n",
 		       b, array[b]);
+}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", a, b);
+
+	for (; a <= min(b, size - 1); a++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+		if (array[a] == value)
+			return (a);
+	}
+
+	return (-1);
+}
